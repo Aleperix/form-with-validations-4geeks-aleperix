ea01a422d233bf23478106aeee69ab5a31d01e03
Aleperix <aleperixx@gmail.com>
2022-09-28 21:04:47 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,52 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let form = document.querySelector("form");
+  form.addEventListener(
+    "submit",
+    function(event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      document.querySelectorAll("input").forEach(element => {
+        if (!element.hasAttribute("required")) {
+          element.setAttribute("required", "required");
+        }
+      });
+      if (!form.hasAttribute("needs-validation")) {
+        form.classList.add("needs-validation");
+      }
+      if (!form.hasAttribute("was-validated")) {
+        form.classList.add("was-validated");
+      }
+    },
+    false
+  );
 };
+
+const getJSON = async url => {
+  const response = await fetch(url);
+  if (!response.ok)
+    // check if response worked (no 404 errors etc...)
+    throw new Error(response.statusText);
+
+  const data = response.json(); // get JSON from the response
+  return data; // returns a promise, which resolves to this data value
+};
+
+console.log("Fetching data...");
+getJSON(
+  "https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json"
+)
+  .then(data => {
+    for (const item of Object.entries(data)) {
+      let opt = document.createElement("option");
+      opt.innerHTML = item[1];
+      opt.value = item[0];
+      document.querySelector("#fState").appendChild(opt);
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
